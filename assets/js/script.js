3bd531d91de616b0a14198b26152c05cc5331a99
JamieT966 <jamietarpey1@gmail.com>
2022-02-06 18:48:13 +0000

    Added function to make quiz game appear and start button disappear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("hello!");
+/* Declaring variables */
+const startButton = document.getElementById('start');
+const quizArea = document.getElementById('quiz-area');
+const question = document.getElementById('question');
+const answerBtns = document.getElementsByClassName('buttons');
+
+function unhideQuiz() {
+    quizArea.classList.remove('hide')
+    startButton.classList.add('hide')
+}
+
+startButton.addEventListener('click', unhideQuiz);
