502de9b08f97e86d3bcff555290b4941f638b43e
JamieT966 <jamietarpey1@gmail.com>
2022-02-07 00:05:31 +0000

    Added styling to buttons and overall quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 const startButton = document.getElementById('start');
 const quizArea = document.getElementById('quiz-area');
 const question = document.getElementById('question');
-const answerBtns = document.getElementsByClassName('buttons');
+const answerBtns = document.getElementsByClassName('choices');
+const nextButton = document.getElementById('next');
 
 // Show quiz area and hide start button
 function unhideQuiz() {
@@ -13,27 +14,22 @@ function unhideQuiz() {
 startButton.addEventListener('click', unhideQuiz);
 
 // Get Question 
-
-
-
-const question = [
+let questions = [
     {
-        question: "What is a bund?"
-        answers: [
-            { text: 'A Secondary Enclosure' correct: true}
-            { text: 'A Car', correct: false},
-            { text: 'A Bottle' correct: false},
-            { text: 'A Gathering' correct: false}
-        ]
+        question: 'What is a bund?',
+        choice1: 'A Car',
+        choice2: 'Office Furniture',
+        choice3: 'A Gathering',
+        choice4: 'A Secondary Enclosure',
+        answer: 4, 
     },
     {
-        question: "After what length of time should a bund be serviced?"
-        answers: [
-            { text: '3 years' correct: true}
-            { text: '5 years', correct: false},
-            { text: '1 year' correct: false},
-            { text: '6 months' correct: false}
-        ]
+        question: 'What is the maximum length of time a bund should be left unserviced?',
+        choice1: '5 years',
+        choice2: '3 years',
+        choice3: '1 year',
+        choice4: '6 months',
+        answer: 2, 
     },
-
 ]
+
