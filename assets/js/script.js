7ca532c49d96d38157e89f00f123db663e6ec36a
JamieT966 <jamietarpey1@gmail.com>
2022-02-08 20:33:53 +0000

    added full set of  questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,12 @@ const quizArea = document.getElementById('quiz-area');
 const questionText = document.getElementById('question-text');
 const answerButtons = Array.from(document.getElementsByClassName('choices'));
 const nextButton = document.getElementById('next-button');
-const answerOne = document.getElementById('answer1');
-
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+const MAX_QUESTIONS = 10;
 
 // Show quiz area and hide start button
 function unhideQuiz() {
@@ -19,8 +23,6 @@ function unhideQuiz() {
 startButton.addEventListener('click', unhideQuiz);
 
 // Get Question 
-let currentQuestion = 0;
-var score = 0;
 
 let questions = [
     {
@@ -37,6 +39,70 @@ let questions = [
         choice2: '1 year', 
         choice3: '3 years', 
         choice4: '5 years',
+        answer: 3
+    },
+    {
+        question: 'What is an employers duty under the Safety, Health & Welfare At Work Act?',
+        choice1: 'Pay above minimum wage', 
+        choice2: 'Prevent exposure to dangerous chemicals', 
+        choice3: 'Turn up to work on time', 
+        choice4: 'Purchase you a company car',
+        answer: 2
+    },
+    {
+        question: 'What percentage of volumne must a bund be able to store?',
+        choice1: '25%', 
+        choice2: '75%', 
+        choice3: '110%', 
+        choice4: '150%',
+        answer: 3
+    },
+    {
+        question: 'What item would you typically find in a spill kit?',
+        choice1: 'Condiments', 
+        choice2: 'Chemicals', 
+        choice3: 'Bandages', 
+        choice4: 'Spill socks',
+        answer: 4
+    },
+    {
+        question: 'What is the maximum length of time a bund should be left unserviced?',
+        choice1: '6 months', 
+        choice2: '1 year', 
+        choice3: '3 years', 
+        choice4: '5 years',
+        answer: 3
+    },
+    {
+        question: 'What should you do in the event of a major fire?',
+        choice1: 'Call 999', 
+        choice2: 'Try to put it out yourself', 
+        choice3: 'Just leave it', 
+        choice4: 'Panic',
+        answer: 1
+    },
+    {
+        question: 'Which of these is NOT an entry route for chemicals into the body?',
+        choice1: 'Inhalation', 
+        choice2: 'Viewing', 
+        choice3: 'Absorption', 
+        choice4: 'Ingestion',
+        answer: 2
+    },
+    {
+        question: 'If a chemical is said to be carcinogenic, what does that mean?',
+        choice1: 'Causes cancer', 
+        choice2: 'Causes adverse health effects', 
+        choice3: 'Causes severe irritation', 
+        choice4: 'Causes genetic mutation',
+        answer: 1
+    },
+    {
+        question: 'What does an SDS stand for?',
+        choice1: 'Solid Drive State', 
+        choice2: 'Secure Device Safety', 
+        choice3: 'Safety Data Sheet', 
+        choice4: 'Safety Demonstration Slide',
         answer: 3
     },
 ]
@@ -76,13 +142,11 @@ function testPassingData(usersChoice, button) {
 } else {
     button.classList.add('incorrect')
 }
+setTimeout(() => {
+    selectedChoice.parentElement.classList.remove(classToApply);
+    getNewQuestion();
+  }, 1000);
 }
-
-/*
-console.log(usersChoice)
-console.log(questions[0].answer)
-*/
-
 
 
 
