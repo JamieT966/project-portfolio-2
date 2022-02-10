919df6d4608012afbc9447ee09f09f925f205c6b
JamieT966 <jamietarpey1@gmail.com>
2022-02-10 01:08:45 +0000

    Fixed typo

--- assets/js/end-quiz.js
+++ assets/js/end-quiz.js
@@ -4,7 +4,7 @@ const resultShow = document.getElementById('result')
 
 
 if(playerFinalScore < 5) {
-    resultShow.innerText = `You scored ${playerFinalScore} of 10! Needs improvement! Please click restart`;
+    resultShow.innerText = `You scored ${playerFinalScore} of 10! Needs improvement! Please click restart.`;
 } else {
     resultShow.innerText = `You scored ${playerFinalScore} of 10! Good work!`;
 }
