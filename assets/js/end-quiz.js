fc0c9ab5244f662ffa70e19e929e1bb8fae8109b
JamieT966 <jamietarpey1@gmail.com>
2022-02-10 02:28:37 +0000

    Beautified all code, JavaScript, CSS and HTML

--- assets/js/end-quiz.js
+++ assets/js/end-quiz.js
@@ -1,10 +1,10 @@
 // Declaring variables
-const playerFinalScore= localStorage.getItem('playerFinalScore');
+const playerFinalScore = localStorage.getItem('playerFinalScore');
 const resultShow = document.getElementById('result')
 /**
  * Takes user's score and gives result and message.
  */
-if(playerFinalScore < 5) {
+if (playerFinalScore < 5) {
     resultShow.innerText = `You scored ${playerFinalScore} of 10! Needs improvement! Please click restart.`;
 } else {
     resultShow.innerText = `You scored ${playerFinalScore} of 10! Good work!`;
--- assets/js/script.js
+++ assets/js/script.js
@@ -31,84 +31,83 @@ function unhideQuiz() {
 }
 
 // Get array of questions
-let questions = [
-    {
+let questions = [{
         question: 'What is a bund?',
-        choice1: 'A Secondary Enclosure', 
-        choice2: 'A Gathering', 
-        choice3: 'Office Furniture', 
+        choice1: 'A Secondary Enclosure',
+        choice2: 'A Gathering',
+        choice3: 'Office Furniture',
         choice4: 'A Car',
         answer: 1
     },
     {
         question: 'What is the maximum length of time a bund should be left unserviced?',
-        choice1: '6 months', 
-        choice2: '1 year', 
-        choice3: '3 years', 
+        choice1: '6 months',
+        choice2: '1 year',
+        choice3: '3 years',
         choice4: '5 years',
         answer: 3
     },
     {
         question: 'What is an employers duty under the Safety, Health & Welfare At Work Act?',
-        choice1: 'Pay above minimum wage', 
-        choice2: 'Prevent exposure to dangerous chemicals', 
-        choice3: 'Turn up to work on time', 
+        choice1: 'Pay above minimum wage',
+        choice2: 'Prevent exposure to dangerous chemicals',
+        choice3: 'Turn up to work on time',
         choice4: 'Purchase you a company car',
         answer: 2
     },
     {
         question: 'What percentage of volumne must a bund be able to store?',
-        choice1: '25%', 
-        choice2: '75%', 
-        choice3: '110%', 
+        choice1: '25%',
+        choice2: '75%',
+        choice3: '110%',
         choice4: '150%',
         answer: 3
     },
     {
         question: 'What item would you typically find in a spill kit?',
-        choice1: 'Condiments', 
-        choice2: 'Chemicals', 
-        choice3: 'Bandages', 
+        choice1: 'Condiments',
+        choice2: 'Chemicals',
+        choice3: 'Bandages',
         choice4: 'Spill socks',
         answer: 4
     },
     {
         question: 'What Chemstore product would you use to store a corrosive chemical?',
-        choice1: 'Firevault', 
-        choice2: 'Gasvault', 
-        choice3: 'Corvault', 
+        choice1: 'Firevault',
+        choice2: 'Gasvault',
+        choice3: 'Corvault',
         choice4: 'Hazvault',
         answer: 3
     },
     {
         question: 'What should you do in the event of a major fire?',
-        choice1: 'Call 999', 
-        choice2: 'Try to put it out yourself', 
-        choice3: 'Just leave it', 
+        choice1: 'Call 999',
+        choice2: 'Try to put it out yourself',
+        choice3: 'Just leave it',
         choice4: 'Panic',
         answer: 1
     },
     {
         question: 'Which of these is NOT an entry route for chemicals into the body?',
-        choice1: 'Inhalation', 
-        choice2: 'Viewing', 
-        choice3: 'Absorption', 
+        choice1: 'Inhalation',
+        choice2: 'Viewing',
+        choice3: 'Absorption',
         choice4: 'Ingestion',
         answer: 2
     },
     {
         question: 'If a chemical is said to be carcinogenic, what does that mean?',
-        choice1: 'Causes cancer', 
-        choice2: 'Causes adverse health effects', 
-        choice3: 'Causes severe irritation', 
+        choice1: 'Causes cancer',
+        choice2: 'Causes adverse health effects',
+        choice3: 'Causes severe irritation',
         choice4: 'Causes genetic mutation',
         answer: 1
     },
     {
         question: 'What does an SDS stand for?',
-        choice1: 'Solid Drive State', 
-        choice2: 'Secure Device Safety', 
-        choice3: 'Safety Data Sheet', 
+        choice1: 'Solid Drive State',
+        choice2: 'Secure Device Safety',
+        choice3: 'Safety Data Sheet',
         choice4: 'Safety Demonstration Slide',
         answer: 3
     },
@@ -125,7 +124,7 @@ function showQuiz() {
     answerButtons[2].textContent = questions[questionCounter].choice3;
     answerButtons[3].textContent = questions[questionCounter].choice4;
     questionProgress++
-    
+
     // Displays current question user is on
     turn.innerText = `Question ${questionProgress} of 10`;
     // Takes user to end-quiz.html
@@ -134,9 +133,10 @@ function showQuiz() {
 
 /**
  * Increments questionCounter, allowing user to change set of questions when next is clicked.
-*/
+ */
 function nextQuestion() {
-    showQuiz(questionCounter++);resetChoice();
+    showQuiz(questionCounter++);
+    resetChoice();
     resetChoice();
     questionText.style.color = '#ffffff';
 }
@@ -157,24 +157,25 @@ answerButtons.forEach(button => button.addEventListener('click', () => {
     let theData = button.dataset.choice;
     testPassingData(theData, button)
 }))
+
 function testPassingData(usersChoice, button) {
     if (usersChoice == questions[questionCounter].answer) {
-    button.classList.toggle('correct')
-    // Increments score for correct answer
-    score++
-    // Disables other buttons from being selected
-    disableChoice()
-    // Changes question text to correct message
-    questionText.innerText = "Well done, you got that question right!";
-    questionText.style.color = "#98c244";
-} else {
-    button.classList.toggle('incorrect')
-    // Disables other buttons from being selected
-    disableChoice()
-     // Changes question text to incorrect message
-     questionText.innerText = "Unlucky, that answer was wrong.";
-     questionText.style.color = "#e6554d";
-}
+        button.classList.toggle('correct')
+        // Increments score for correct answer
+        score++
+        // Disables other buttons from being selected
+        disableChoice()
+        // Changes question text to correct message
+        questionText.innerText = "Well done, you got that question right!";
+        questionText.style.color = "#98c244";
+    } else {
+        button.classList.toggle('incorrect')
+        // Disables other buttons from being selected
+        disableChoice()
+        // Changes question text to incorrect message
+        questionText.innerText = "Unlucky, that answer was wrong.";
+        questionText.style.color = "#e6554d";
+    }
 }
 /**
  * Disables all other buttons when choice has been made to avoid multiple guesses.
@@ -217,10 +218,10 @@ function clearAnswer() {
 }
 /**
  * Checks if question progress is equal to max questions i.e, does 10 = 10. If yes run endPage function
-*/
+ */
 function endQuiz() {
-    if (questionProgress === MAX_QUESTIONS) { 
-       nextButton.addEventListener('click', endPage)
+    if (questionProgress === MAX_QUESTIONS) {
+        nextButton.addEventListener('click', endPage)
     }
 }
 /**
@@ -234,18 +235,21 @@ function endPage() {
 
 /**
  *  Entire modal structure taken from W3 Schools
- */ 
+ */
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
+
 function modalBtn() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
+
 function modalSpan() {
-  modal.style.display = "none";
-}
-function modalWindow(event) {
-  if (event.target == modal) {
     modal.style.display = "none";
-  }
+}
+
+function modalWindow(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
